2b49f7b03d2e754548ecf7bab441fc892d12067d
Sheng Xiao <sheng26v@gmail.com>
2020-08-22 15:38:47 -0700

    feat: product service

---
+++ core/services/productService.js
@@ -0,0 +1,24 @@
+const productModel = require('../model/productModel');
+
+const getProduct = (req, res) => {
+  return res.status(200).json('cool!!!');
+}
+
+const createProduct = (req, res) => {
+
+}
+
+const updateProduct = (req, res) => {
+
+}
+
+const deleteProduct = (req, res) => {
+
+}
+
+module.exports = {
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+}
