bcc1e8139d66277dd112f41e5fb145775703d197
Sheng Xiao <sheng26v@gmail.com>
2020-08-22 15:39:29 -0700

    feat: initial app services

---
+++ core/app.js
@@ -0,0 +1,30 @@
+const app = require('express')();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const productRouter = require('./routes/product');
+const categoryRouter = require('./routes/category');
+
+// application port
+const PORT = 3000;
+// mongodb connection options
+const options = {
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  connectTimeoutMS: 10000,
+};
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/api/product', productRouter);
+app.use('/api/category', categoryRouter);
+
+mongoose.connect(process.env.MONGODB_URI, options).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Listen to port ${PORT}`);
+  });  
+});
