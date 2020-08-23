8fd6cc5a54d17744a78a29b4db5efe4c90af4a1b
Sheng Xiao <sheng26v@gmail.com>
2020-08-23 10:34:41 -0700

    feat: store model as a profile for the store

---
+++ core/model/storeModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const storeSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  categories: {
+    type: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+    }],
+    default: [],
+  },
+  products: {
+    type: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+    }],
+    default: [],
+  },
+  tax: {
+    enable: {
+      type: Boolean,
+      default: false
+    },
+    rate: {
+      type: Number,
+      default: 0.0
+    }
+  }
+});
+
+const storeModel = mongoose.model('Store', storeSchema);
+
+module.exports = storeModel;
