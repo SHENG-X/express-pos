229f7d860ee96e3de2c154095e7a8a0913ba9ebc
Sheng Xiao <sheng26v@gmail.com>
2020-08-23 11:13:06 -0700

    chore: store is required for get and create a category

--- core/services/categoryService.js
+++ core/services/categoryService.js
@@ -1,40 +1,67 @@
 const categoryModel = require('../model/categoryModel');
+const storeModel = require('../model/storeModel');
 
 const getCategory = (req, res) => {
+  const store = req.query.store;
   const categoryId = req.query.id;
-  if (categoryId) {
-    // category id was set, then fetch single category by id
-    return categoryModel.find({_id: categoryId}, (error, category) => {
-      if (error) {
-        return res.status(500).json(error);
-      }
-
-      if (category) {
-        return res.status(200).json(category); 
-      }
-
-      return res.status(404).json(null);
-    });
+  if (!store) {
+    return res.status(400).json(store);
   }
-
-  return categoryModel.find({}, (error, categories) => {
+  return storeModel.findById(store, (error, storeData) => {
     if (error) {
       return res.status(500).json(error);
     }
-    // send all categories as response
-    return res.status(200).json(categories);
+    if (!storeData) {
+      return res.status(400).json(storeData);
+    }
+
+    if (categoryId) {
+      // category id was set, then fetch single category by id
+      return categoryModel.findById(categoryId, (error, category) => {
+        if (error) {
+          return res.status(500).json(error);
+        }
+  
+        if (category) {
+          return res.status(200).json(category); 
+        }
+  
+        return res.status(404).json(null);
+      });
+    }
+  
+    return categoryModel.find({}, (error, categories) => {
+      if (error) {
+        return res.status(500).json(error);
+      }
+      // send all categories as response
+      return res.status(200).json(categories);
+    });
   });
 
 }
 
 const createCategory = (req, res) => {
-  const { thumbnail, name } = req.body;
-  const category = new categoryModel({ thumbnail, name });
-  return category.save((error, category)=> {
+  const { thumbnail, name, store } = req.body;
+  // check if it is a valid store
+  return storeModel.findById(store, (error, storeData) => {
     if (error) {
-      res.status(500).json(error);
+      return res.status(500).json(error);
     }
-    return res.status(201).json(category._doc);
+    if (!storeData) {
+      // store is invalid
+      return res.status(400).json(storeData);
+    }
+    // TODO: do not allow duplicated category
+    // validate by store and name
+    const category = new categoryModel({ thumbnail, name, store});
+    return category.save((error, categoryData) => {
+      if (error) {
+        res.status(500).json(error);
+      }
+
+      return res.status(201).json(categoryData._doc);
+    });
   });
 }
 
