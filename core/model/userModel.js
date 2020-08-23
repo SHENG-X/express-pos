fbb74cbe37b82f379be5711eb2413ab86c5c02f6
Sheng Xiao <sheng26v@gmail.com>
2020-08-22 18:13:13 -0700

    feat: user model

---
+++ core/model/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  enable: {
+    type: Boolean,
+    default: true,
+  }
+});
+
+const userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
