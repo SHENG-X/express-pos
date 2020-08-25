43e9486757423ed42c52a84b07b73ea53c600b43
Sheng Xiao <sheng26v@gmail.com>
2020-08-25 10:29:33 -0700

    chore: products with multiple price options

--- front/src/pages/modules/components/Products.js
+++ front/src/pages/modules/components/Products.js
@@ -19,23 +19,23 @@ const mockProducts = [
     _id: '2314124124131232214',
     name: 'Chai Tea Latte',
     count: 1,
-    price: 12.45
+    prices: [12.45, 10.31, 8.24]
   },
   {
     _id: '2314123412413654121',
     name: 'Milk Latte',
     count: 1,
-    price: 8.45
+    prices: [8.24, 7.85, 6.24]
   },
   {
     _id: '2986753412413654121',
     name: 'Dark Roast',
-    count: 2,
-    price: 4.45
+    count: 1,
+    prices: [4.45, 3.98, 3.56]
   },
 ];
 
-const Products = () => {
+const Products = ({ handleOpen }) => {
   const [products, setProducts] = useState(mockProducts);
 
   return (
@@ -53,7 +53,7 @@ const Products = () => {
       <div className="list-container">
         <div className="list">
           {
-            products.map(prod => <ProductItem product={prod} key={prod._id}/>)
+            products.map(prod => <ProductItem product={prod} handleOpen={handleOpen} key={prod._id}/>)
           }
         </div>
       </div>
@@ -61,10 +61,11 @@ const Products = () => {
   );
 }
 
-const ProductItem = ({ product }) => {
+const ProductItem = ({ product, handleOpen }) => {
   return (
     <ButtonBase
       className="card-button"
+      onClick={() => handleOpen(product)}
     >
       <Card className="card">
         <CardMedia
