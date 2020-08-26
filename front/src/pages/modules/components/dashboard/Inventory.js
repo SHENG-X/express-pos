4c57b29be4d3f4480dc82374b184ef9ba4626632
Sheng Xiao <sheng26v@gmail.com>
2020-08-25 23:22:29 -0700

    feat: initialize inventory report tab

---
+++ front/src/pages/modules/components/dashboard/Inventory.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Typography
+} from '@material-ui/core';
+
+const InventoryReport = () => {
+  return (
+    <div className="sale-tab">
+      <Typography variant="h4">
+        Inventory Report Coming soon
+      </Typography>
+    </div>
+  );
+}
+
+export default InventoryReport;
