75bac7f42632fbc3c47918c438731856ff5cdedb
Sheng Xiao <sheng26v@gmail.com>
2020-08-25 23:22:12 -0700

    feat: initialize sale report tab

---
+++ front/src/pages/modules/components/dashboard/Sale.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Typography
+} from '@material-ui/core';
+
+const SaleReport = () => {
+  return (
+    <div className="sale-tab">
+      <Typography variant="h4">
+        Sale Report Coming soon
+      </Typography>
+    </div>
+  );
+}
+
+export default SaleReport;
