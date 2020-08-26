b588b6be9383eba504526c9aa0da384514b65b61
Sheng Xiao <sheng26v@gmail.com>
2020-08-25 23:24:14 -0700

    feat: initialize modal base component for common modal

---
+++ front/src/pages/modules/components/ModalBase.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ModalBase = ({ children }) => {
+  return (
+    <div className="carpet">
+      <div className="heading">
+
+      </div>
+      <div className="content">
+
+      </div>
+      <div className="actions">
+
+      </div>
+    </div>
+  );
+}
