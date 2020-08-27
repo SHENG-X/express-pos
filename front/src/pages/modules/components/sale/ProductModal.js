9de4023586e4f04f261518e4b6a42ff524f0208b
Sheng Xiao <sheng26v@gmail.com>
2020-08-26 17:55:35 -0700

    feat: sale page product modal

---
+++ front/src/pages/modules/components/sale/ProductModal.js
@@ -0,0 +1,111 @@
+import React, {
+  useState
+} from 'react';
+import {
+  Typography,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  IconButton
+} from '@material-ui/core';
+import {
+  Add,
+  Remove
+} from '@material-ui/icons';
+
+import ModalBase from '../ModalBase';
+
+const ProductModal = ({ selectedProduct, handleOpen, handleConfirm }) => {
+  const [product, setProduct] = useState(selectedProduct);
+  const addRemoveProduct = (action) => {
+    let base = 1;
+    if (action === 'remove') {
+      base = -1;
+    }
+    if (product.count + base < 1) {
+      return;
+    }
+    setProduct({...product, count: product.count + base})
+  }
+
+  return (
+    <ModalBase
+      title="Add a product"
+      className="product-modal"
+      content={
+        <React.Fragment>
+          <div className="count row">
+            <div className="label">
+            <Typography variant="subtitle1">
+              Quantity
+            </Typography>
+            </div>
+            <div className="action">
+              <IconButton
+                color="primary"
+                size="small"
+                aria-label="add one"
+                onClick={() => addRemoveProduct('add')}
+              >
+                <Add />
+              </IconButton>
+              <Typography variant="subtitle2" align="center" className="count">
+                { product.count }
+              </Typography>
+              <IconButton
+                color="primary"
+                size="small"
+                aria-label="remove one"
+                onClick={() => addRemoveProduct('remove')}
+              >
+                <Remove />
+              </IconButton>
+            </div>
+          </div>
+          <div className="price row">
+            <div className="label">
+              <Typography variant="subtitle1">
+                Price
+              </Typography>
+            </div>
+            <div className="action">
+              <FormControl variant="outlined" >
+                <InputLabel id="demo-simple-select-outlined-label">Price</InputLabel>
+                <Select
+                  labelId="demo-simple-select-outlined-label"
+                  id="demo-simple-select-outlined"
+                  value={product.price}
+                  onChange={(e) => setProduct({...product, price: e.target.value})}
+                  label="Price"
+                >
+                  {
+                    product.prices.map(p => (<MenuItem value={p} key={p}>{p}</MenuItem>))
+                  }
+                </Select>
+              </FormControl>
+            </div>
+          </div>
+        </React.Fragment>
+      }
+      actions={
+        <React.Fragment>
+          <Button 
+            onClick={() => handleOpen(false)}
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={() => handleConfirm(product)}
+          >
+            Confirm
+          </Button>
+        </React.Fragment>
+      }
+    />
+  );
+}
+
+export default ProductModal;
