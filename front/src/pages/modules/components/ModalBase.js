a30c540e56f03648a38d81e96642e3f2b8dcb865
Sheng Xiao <sheng26v@gmail.com>
2020-08-26 17:51:38 -0700

    chore: modal base component takes two children components

--- front/src/pages/modules/components/ModalBase.js
+++ front/src/pages/modules/components/ModalBase.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import {
+  Typography,
+  Paper
+} from '@material-ui/core'
 
-const ModalBase = ({ children }) => {
+const ModalBase = ({ content, actions, title, className }) => {
   return (
-    <div className="carpet">
-      <div className="heading">
-
-      </div>
-      <div className="content">
-
-      </div>
-      <div className="actions">
-
-      </div>
+    <div className={`carpet ${className}`}>
+      <Paper elevation={3}>
+        <div className="heading">
+          <Typography variant="h5">
+            { title }
+          </Typography>
+        </div>
+        <div className="content">
+          { content }
+        </div>
+        <div className="actions">
+          { actions }
+        </div>
+      </Paper>
     </div>
   );
 }
+
+export default ModalBase;
