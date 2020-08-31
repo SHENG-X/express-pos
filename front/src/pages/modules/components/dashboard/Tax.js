1be7a57bd289e4dc7cf186edc88ee029b1c09da0
Sheng Xiao <sheng26v@gmail.com>
2020-08-30 21:54:27 -0700

    chore: i18n on tax panel

--- front/src/pages/modules/components/dashboard/Tax.js
+++ front/src/pages/modules/components/dashboard/Tax.js
@@ -7,25 +7,28 @@ import {
   Switch,
   TextField,
   Typography,
-} from '@material-ui/core'
+} from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
+
 import { Context } from '../../../../context/storeContext';
 
 const Tax = () => {
   const { state, updateTax } = useContext(Context);
+  const { t } = useTranslation();
 
   return (
     <Paper elevation={3}>
       <div className="tax-tab">
           <div className="heading">
             <Typography variant="subtitle1">
-              Tax
+              { t('tax.heading') }
             </Typography>
           </div>
           <div className="content">
             <div className="row">
               <div className="label">
                 <Typography variant="subtitle1">
-                  Enable
+                  { t('common.enable') }
                 </Typography>
               </div>
               <div className="input">
@@ -33,7 +36,6 @@ const Tax = () => {
                   checked={state.store.tax.enable}
                   onChange={(e, val) => updateTax({tax: {...state.store.tax, enable: val}, store: state.store._id})}
                   color="primary"
-                  name="Tax enable"
                   inputProps={{ 'aria-label': 'primary checkbox' }}
                 />
               </div>
@@ -41,14 +43,14 @@ const Tax = () => {
             <div className="row">
               <div className="label">
                 <Typography variant="subtitle1">
-                  Rate
+                  { t('tax.rate') }
                 </Typography>
               </div>
               <div className="input">
                 <TextField
                   required
                   type="number"
-                  placeholder="Tax rate"
+                  placeholder={ t('tax.taxRate') }
                   value={state.store.tax.rate}
                   onChange={e => updateTax({ tax: {...state.store.tax, rate: e.target.value}, store: state.store._id})}
                   inputProps={{step: 0.01}}
