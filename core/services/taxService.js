00cdd0d71208c7c34c22cb6cd6e785279862f3b9
Sheng Xiao <sheng26v@gmail.com>
2020-09-05 16:16:45 -0700

    chore: update tax service use async and await

--- core/services/taxService.js
+++ core/services/taxService.js
@@ -1,20 +1,15 @@
 const storeModel = require('../model/storeModel');
 
-const updateTax = (req, res) => {
-  const { store, tax } = req.body;
-  return storeModel.findById(store, (error, store) => {
-    if (error) {
-      return res.status(500).json(error);
-    }
-    // update tax to new tax
-    store.tax = tax;
-    return store.save((error, store) => {
-      if (error) {
-        return res.status(500).json(error);
-      }
-      return res.status(200).json(store._doc.tax);
-    });
-  });
+const updateTax = async (req, res) => {
+  const storeId = req.decoded.store;
+  const { tax } = req.body;
+  try {
+    const updatedStore = await storeModel.findOneAndUpdate(storeId, { tax }, { new : true });
+    const updatedTaxDoc = updatedStore._doc.tax;
+    return res.status(200).json(updatedTaxDoc);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 }
 
 module.exports = {
