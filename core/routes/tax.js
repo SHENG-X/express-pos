e53d4f34503027cf6c8d2da2ffe301bff8c40ea4
Sheng Xiao <sheng26v@gmail.com>
2020-09-05 15:25:31 -0700

    chore: no need for get tax since it is a property of store

--- core/routes/tax.js
+++ core/routes/tax.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getTax, updateTax } = require('../services/taxService');
-const { update } = require('../model/userModel');
-
-router.get('/', getTax);
+const { updateTax } = require('../services/taxService');
 
 router.put('/', updateTax);
 
--- core/services/taxService.js
+++ core/services/taxService.js
@@ -1,18 +1,5 @@
 const storeModel = require('../model/storeModel');
 
-const getTax = (req, res) => {
-  const store = req.query.store;
-  if (!store) {
-    return res.status(400).json(store);
-  }
-  return storeModel.findById(store, (error, store) => {
-    if (error) {
-      return res.status(500).json(error);
-    }
-    return res.status(200).json(store._doc.tax);
-  });
-}
-
 const updateTax = (req, res) => {
   const { store, tax } = req.body;
   return storeModel.findById(store, (error, store) => {
@@ -31,6 +18,5 @@ const updateTax = (req, res) => {
 }
 
 module.exports = {
-  getTax,
   updateTax,
 }
