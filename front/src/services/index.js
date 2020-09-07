24755c7f5fb07936aaacfe6c0c0a9ec2e0fda8f0
Sheng Xiao <sheng26v@gmail.com>
2020-09-06 23:02:36 -0700

    fix: correct the way of set token
    - add token before each request send to the server side

--- front/src/services/index.js
+++ front/src/services/index.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-const token = localStorage.getItem('EXPRESS-POS/token');
-
 const instance = axios.create({
-  headers: {
-    Authorization: 'Bearer ' + (token ? token : '') 
-  },
   baseURL: 'http://localhost:3000',
+});
+
+// Set the AUTH token for any request
+instance.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('EXPRESS-POS/token');
+  config.headers.Authorization =  token ? `Bearer ${token}` : '';
+  return config;
 });
 
 export const register = async (name, email, password) => {
