dbc3b3b8c2a535d05b42cf767f57655af1f30ebc
Sheng Xiao <sheng26v@gmail.com>
2020-09-09 18:47:00 -0700

    feat: inventory summary section in the dashboard inventory tab

--- front/src/pages/modules/components/dashboard/Inventory.js
+++ front/src/pages/modules/components/dashboard/Inventory.js
@@ -1,15 +1,87 @@
-import React from 'react';
+import React, {
+  useContext,
+} from 'react';
 import {
-  Typography
+  Typography,
+  Paper,
 } from '@material-ui/core';
+import { Context } from '../../../../context/storeContext';
 
 const InventoryReport = () => {
   return (
     <div className="sale-tab">
-      <Typography variant="h4">
-        Inventory Report Coming soon
-      </Typography>
+      <InventorySummary/>
     </div>
+  );
+}
+
+const InventorySummary = () => {
+  const { state } = useContext(Context);
+
+  const computeProducts = () => {
+    return state.store.products.reduce((acc, prod) => acc + prod.count, 0);
+  }
+
+  const computeDisabledProducts = () => {
+    return state.store.products.reduce((acc, prod) => acc + (prod.enable ? 0 : 1), 0);
+  }
+
+  const computeCategories = () => {
+    return state.store.categories.length;
+  }
+
+  return (
+    <Paper
+      elevation={3}
+      className="inventory-summary"
+    > 
+      <div className="heading">
+        <Typography variant="subtitle1">
+          Inventory Summary
+        </Typography>
+      </div>
+
+      <div className="content">
+        <div className="row">
+          <div className="label">
+            <Typography variant="body2">
+              All Products
+            </Typography>
+          </div>
+          <div className="value">
+            <Typography variant="subtitle2">
+              { computeProducts() }
+            </Typography>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="label">
+            <Typography variant="body2">
+              Disabled Products
+            </Typography>
+          </div>
+          <div className="value">
+            <Typography variant="subtitle2">
+              { computeDisabledProducts() }
+            </Typography>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="label">
+            <Typography variant="body2">
+              All Categories
+            </Typography>
+          </div>
+          <div className="value">
+            <Typography variant="subtitle2">
+              { computeCategories() }
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </Paper>
   );
 }
 
