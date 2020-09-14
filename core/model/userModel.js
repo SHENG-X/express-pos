09bb056c2b3f907992e3df5885399e21a910a867
Sheng Xiao <sheng26v@gmail.com>
2020-09-14 09:31:11 -0700

    feat: user has a staff number which is auto incremented

--- core/model/userModel.js
+++ core/model/userModel.js
@@ -37,12 +37,42 @@ const userSchema = new Schema(
     phone: {
       type: Number,
       required: true,
+    },
+    staffNo: {
+      type: Number,
+      unique: true,
+      min: 1,
     }
   },
   {
     timestamps: true,
   },
 );
+
+// use middleware to increase staff number 
+// on new staff added
+userSchema.pre('save', async function (next) {
+  if (!this.isNew) {
+    next();
+    return;
+  }
+  // if a new staff is added
+  const staff = await userModel.find({ store: this.store.toString() });
+  let max = 1;
+  if (staff.length) {
+    // more than one staff
+    staff.forEach(stf => {
+      if (stf.staffNo > max) {
+        max = stf.staffNo;
+      }
+    });
+    // max is the previous maximum employee number
+    // increase max by one for the new employee number
+    max += 1;
+  }
+  this.staffNo = max;
+  next();
+});
 
 const userModel = mongoose.model('User', userSchema);
 
