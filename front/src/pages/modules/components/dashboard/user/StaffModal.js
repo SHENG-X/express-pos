557ddb3270b9672f3b8fb13ecb18f5f8a61e3b5d
Sheng Xiao <sheng26v@gmail.com>
2020-09-16 15:45:15 -0700

    chore: staff modal use formik package for form validation

--- front/src/pages/modules/components/dashboard/user/StaffModal.js
+++ front/src/pages/modules/components/dashboard/user/StaffModal.js
@@ -4,20 +4,28 @@ import React, {
 } from 'react';
 import {
   Typography,
-  TextField,
   Button,
-  Select,
   MenuItem,
-  FormControl,
-  InputLabel,
-  Switch,
-  Input,
+  CircularProgress,
 } from '@material-ui/core';
 import { useToasts } from 'react-toast-notifications';
+import {Formik, Form, Field} from 'formik';
+import {
+  TextField,
+  Switch,
+} from 'formik-material-ui';
 
-import ModalBase from '../../ModalBase';
+import ModalBaseV2 from '../../ModalBaseV2';
 import { Context } from '../../../../../context/userContext';
-import PhoneNumMask from '../../PhoneNumMask';
+import { 
+  validateName,
+  validatePhone,
+  validateEmail,
+  validatePassword,
+ } from '../../../form/validation';
+ import LowerCaseTextField from '../../../formikTextField/LowerCaseTextField';
+ import PhoneNumberTextField from '../../../formikTextField/PhoneNumberTextField';
+
 
 const StaffModal = ({ handleOpen, currentStaff, resetCurrentStaff }) => {
   let defaultStaff = {
@@ -30,44 +38,50 @@ const StaffModal = ({ handleOpen, currentStaff, resetCurrentStaff }) => {
     email: '',
     password: '',
   };
+
   if (currentStaff) {
     // current staff was set, staff modal should be in update mode
     defaultStaff = { ...currentStaff };
   }
-  const [staff, setStaff] = useState({ ...defaultStaff });
+
+  const [ staff ] = useState({ ...defaultStaff });
   const { userState, addStaff, updateStaff } = useContext(Context);
   const { addToast } = useToasts();
 
-  const handleConfirm = () => {
-    let staffSubmit  = JSON.parse(JSON.stringify(staff));
-    staffSubmit = { ...staffSubmit,  phone: Number(`${staffSubmit.phone}`.match(/\d/g)?.join('')) };
+  const handleConfirm = (values, completeSubmit) => {
+    values = { ...values, phone: Number(`${values.phone}`.match(/\d/g)?.join(''))}
     if (currentStaff) {
-      if (JSON.stringify(staff) === JSON.stringify(currentStaff)) {
+      if (JSON.stringify(values) === JSON.stringify(currentStaff)) {
+        completeSubmit();
         resetCurrentStaff();
         handleOpen(false);
         return;
       }
       updateStaff(
-        staffSubmit,
+        values,
         () => {
+          completeSubmit();
           addToast('Staff updated', { appearance: 'success' });
           // close modal window on staff added
           resetCurrentStaff();
           handleOpen(false);
         },
         () => {
+          completeSubmit();
           addToast('Unable to update the staff', { appearance: 'error' });
         }
       );
     } else {
       addStaff(
-        staffSubmit,
+        values,
         () => {
+          completeSubmit();
           addToast('New staff was added', { appearance: 'success' });
           // close modal window on staff added
           handleOpen(false);
         },
         () => {
+          completeSubmit();
           addToast('Unable to add the staff, please try again later', { appearance: 'error' });
         }
       );
@@ -75,159 +89,231 @@ const StaffModal = ({ handleOpen, currentStaff, resetCurrentStaff }) => {
   }
 
   return (
-    <ModalBase
+    <ModalBaseV2
       title="Add a staff"
       className="staff-modal"
-      content={
-        <React.Fragment>
-          <div className="row">
-            <div className="label">
-              <Typography variant="subtitle2">
-                { 'Role' }
-              </Typography>
-            </div>
-            <div className="input">
-              <FormControl variant="outlined">
-                <InputLabel>{ 'Role' }</InputLabel>
-                <Select
-                  value={ staff.role }
-                  onChange={e => setStaff({...staff, role: e.target.value})}
-                  label={'Role'}
-                >
-                  {
-                    userState.role === 'Owner' &&
-                    <MenuItem value={'Manager'}>
-                      Manager
-                    </MenuItem>
-                  }
-                  <MenuItem value={'Employee'}>
-                    Employee
-                  </MenuItem>
-                </Select>
-              </FormControl>
-            </div>
-          </div>
-          
-          {
-            currentStaff &&
-            <div className="row">
-              <div className="label">
-                <Typography variant="subtitle2">
-                  Enable
-                </Typography>
-              </div>
-              <div className="input">
-              <Switch
-                checked={staff.enable}
-                onChange={(e, val) => setStaff({...staff, enable: val})}
-                color="primary"
-              />
-              </div>
-            </div>
-          }
-
-          <div className="row">
-            <div className="label">
-              <Typography variant="subtitle2">
-                First Name
-              </Typography>
-            </div>
-            <div className="input">
-              <TextField
-                placeholder="First name"
-                value={staff.fname}
-                onChange={e => setStaff({...staff, fname: e.target.value})}
-              />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="label">
-              <Typography variant="subtitle2">
-                Last Name
-              </Typography>
-            </div>
-            <div className="input">
-              <TextField
-                placeholder="Last name"
-                value={staff.lname}
-                onChange={e => setStaff({...staff, lname: e.target.value})}
-              />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="label">
-              <Typography variant="subtitle2">
-                Phone Number
-              </Typography>
-            </div>
-            <div className="input">
-              <Input
-                value={staff.phone}
-                onChange={e => setStaff({...staff, phone: e.target.value})}
-                inputComponent={PhoneNumMask}
-              />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="label">
-              <Typography variant="subtitle2">
-                Email
-              </Typography>
-            </div>
-            <div className="input">
-              <TextField
-                type="email"
-                placeholder="Email"
-                value={staff.email}
-                onChange={e => setStaff({...staff, email: e.target.value})}
-              />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="label">
-              <Typography variant="subtitle2">
-                Password
-              </Typography>
-            </div>
-            <div className="input">
-              <TextField
-                type="password"
-                placeholder="Password"
-                value={staff.password}
-                onChange={e => setStaff({...staff, password: e.target.value})}
-              />
-            </div>
-          </div>
-
-        </React.Fragment>
-      }
-      actions={
-        <React.Fragment>
-          <Button
-            variant="contained"
-            onClick={() => {
-              if (currentStaff) {
-                resetCurrentStaff();
+    >
+      <Formik
+        initialValues={staff}
+        validate={(values) => {
+          const errors =  {};
+          if (currentStaff) {
+            // validate for update
+            if (values.fname !== currentStaff.fname) {
+              if (validateName(values.fname)) {
+                errors.fname = validateName(values.fname);
               }
-              handleOpen(false)
-            }}
-          >
-            Cancel
-          </Button>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={handleConfirm}
-          >
-            Confirm
-          </Button>
-        </React.Fragment>
-      }
-    />
+            }
+            if (values.lname !== currentStaff.lname) {
+              if (validateName(values.lname)) {
+                errors.lname = validateName(values.lname);
+              }
+            }
+            if (values.email !== currentStaff.email) {
+              if (validateEmail(values.email)) {
+                errors.email = validateEmail(values.email);
+              }
+            }
+            if (Number(`${values.phone}`?.match(/\d/g)?.join('')) !== Number(currentStaff.phone)) {
+              if (validatePhone(values.phone)) {
+                errors.phone = validatePhone(values.phone);
+              }
+            }
+            if (values.password !== currentStaff.password) {
+              if (validatePassword(values.password)) {
+                errors.password = validatePassword(values.password);
+              }
+            }
+            
+          } else {
+            if (validateName(values.fname)) {
+              errors.fname = validateName(values.fname);
+            }
+            if (validateName(values.lname)) {
+              errors.lname = validateName(values.lname);
+            }
+            if (validateEmail(values.email)) {
+              errors.email = validateEmail(values.email);
+            }
+            if (validatePhone(values.phone)) {
+              errors.phone = validatePhone(values.phone);
+            }
+            if (validatePassword(values.password)) {
+              errors.password = validatePassword(values.password);
+            }
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          handleConfirm(values, () => setSubmitting(false));
+        }}
+      >
+        {({ submitForm, isSubmitting }) => (
+          <Form>
+            <React.Fragment>
+
+              <div className="content">
+                <div className="row">
+                  <div className="label">
+                    <Typography variant="subtitle2">
+                      { 'Role' }
+                    </Typography>
+                  </div>
+                  <div className="input">
+                    <Field
+                      component={TextField}
+                      type="text"
+                      name="role"
+                      select
+                      variant="outlined"
+                    >
+                      {
+                        userState.role === 'Owner' &&
+                        <MenuItem value={'Manager'}>
+                          Manager
+                        </MenuItem>
+                      }
+                      <MenuItem value={'Employee'}>
+                        Employee
+                      </MenuItem>
+                    </Field>
+                  </div>
+                </div>
+                
+                {
+                  currentStaff &&
+                  <div className="row">
+                    <div className="label">
+                      <Typography variant="subtitle2">
+                        Enable
+                      </Typography>
+                    </div>
+                    <div className="input">
+                      <Field
+                        component={Switch}
+                        type="checkbox"
+                        name="enable"
+                        color="primary"
+                      />
+                    </div>
+                  </div>
+                } 
+
+                <div className="row">
+                  <div className="label">
+                    <Typography variant="subtitle2">
+                      First Name
+                    </Typography>
+                  </div>
+                  <div className="input">
+                    <Field
+                      component={TextField}
+                      name="fname"
+                      placeholder="First name"
+                      autoComplete='new-fname'
+                    />
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="label">
+                    <Typography variant="subtitle2">
+                      Last Name
+                    </Typography>
+                  </div>
+                  <div className="input">
+                    <Field
+                      component={TextField}
+                      name="lname"
+                      placeholder="Last name"
+                      autoComplete='new-lname'
+                    />
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="label">
+                    <Typography variant="subtitle2">
+                      Phone Number
+                    </Typography>
+                  </div>
+                  <div className="input">
+                    <Field
+                      component={PhoneNumberTextField}
+                      type="tel"
+                      name="phone"
+                      autoComplete='new-phone'
+                    />
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="label">
+                    <Typography variant="subtitle2">
+                      Email
+                    </Typography>
+                  </div>
+                  <div className="input">
+                    <Field
+                      component={LowerCaseTextField}
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      autoComplete='new-email'
+                    />
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="label">
+                    <Typography variant="subtitle2">
+                      Password
+                    </Typography>
+                  </div>
+                  <div className="input">
+                    <Field
+                      component={TextField}
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      autoComplete='new-password'
+                    />
+                  </div>
+                </div>
+              </div>
+              {
+                  isSubmitting &&
+                  <div className="flex justify-center">
+                    <CircularProgress />
+                  </div>
+              }
+              <div className="actions">
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    if (currentStaff) {
+                      resetCurrentStaff();
+                    }
+                    handleOpen(false)
+                  }}
+                  disabled={isSubmitting}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  onClick={submitForm}
+                  disabled={isSubmitting}
+                >
+                  Confirm
+                </Button>
+            </div>
+            </React.Fragment>
+          </Form>
+        )}
+      </Formik>
+    </ModalBaseV2>
   )
 }
 
