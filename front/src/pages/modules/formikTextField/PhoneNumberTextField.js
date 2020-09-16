b8441a1ef002e4429bb53799c896534d4fe247cf
Sheng Xiao <sheng26v@gmail.com>
2020-09-16 15:43:42 -0700

    feat: custom formik textfield for phone number input

---
+++ front/src/pages/modules/formikTextField/PhoneNumberTextField.js
@@ -0,0 +1,41 @@
+import React, {
+  useCallback,
+} from 'react';
+import {
+  fieldToTextField,
+} from 'formik-material-ui';
+import {
+  Input,
+  FormHelperText,
+} from '@material-ui/core';
+
+import PhoneNumMask from '../components/PhoneNumMask';
+
+const PhoneNumberTextField = (props) => {
+  const {
+    form: {setFieldValue},
+    field: {name},
+  } = props;
+  const onChange = useCallback(
+    (event) => {
+      const {value} = event.target;
+      setFieldValue(name, value ? value.toLowerCase() : '');
+    },
+    [setFieldValue, name]
+  );
+
+  const textFieldProps = fieldToTextField(props);
+  return (
+    <React.Fragment>
+      <Input inputComponent={PhoneNumMask} {...fieldToTextField(props)} onChange={onChange} />
+      {
+        textFieldProps.error &&
+        <FormHelperText error>
+          { textFieldProps.helperText }
+        </FormHelperText>
+      }
+    </React.Fragment>
+  );
+}
+
+export default PhoneNumberTextField;
