754509640b2b13ef0c23c97fde8f6536bab20328
Sheng Xiao <sheng26v@gmail.com>
2020-09-16 15:44:29 -0700

    feat: validation handler for name, email, phone, password

--- front/src/pages/modules/form/validation.js
+++ front/src/pages/modules/form/validation.js
@@ -21,3 +21,39 @@ export function required(requiredFields, values, message) {
     {},
   );
 }
+
+export const validateName = (name) => {
+  if (!name) {
+    return 'Required';
+  } else if (name.length < 3) {
+    return 'At least 3 characters'
+  }
+}
+
+export const validatePhone = (phone) => {
+  if (!phone) {
+    return 'Required';
+  } else if (`${phone}`.match(/\d/g)?.join('').length !== 10) {
+    return 'Invalid phone number'
+  }
+}
+
+export const validateEmail = (email) => {
+  if (!email) {
+    return 'Required';
+  } else if (
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)
+  ) {
+    return 'Invalid email address';
+  }
+}
+
+export const validatePassword = (password) => {
+  // for now only require more than 4 characters
+  // TODO: validate with strong password schema
+  if (!password){
+    return 'Required';
+  } else if (password.length < 4) {
+    return 'At least 4 characters';
+  }
+}
