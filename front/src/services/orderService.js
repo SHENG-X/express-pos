1ec1af8a98100da09837f38313a87ee3917a57ed
Sheng Xiao <sheng26v@gmail.com>
2020-09-17 21:43:31 -0700

    chore: move client side order related service to order service file

---
+++ front/src/services/orderService.js
@@ -0,0 +1,25 @@
+import instance from './index';
+
+export const getOrderAsync = async (oid, startDate, endDate) => {
+  try {
+    return await instance.get('/api/order', { params: { oid, startDate, endDate } });
+  } catch (error) {
+    return error.response;
+  }
+}
+
+export const createOrderAsync = async (order) => {
+  try {
+    return await instance.post('/api/order', { ...order });
+  } catch (error) {
+    return error.response;
+  }
+}
+
+export const deleteOrderAsync = async (order) => {
+  try {
+    return await instance.delete('/api/order', { params: {...order} });
+  } catch (error) {
+    return error.response;
+  }
+}
