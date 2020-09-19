6df7ad5ee3ce640a3250497615f9814c6ff4afa0
Sheng Xiao <sheng26v@gmail.com>
2020-09-18 20:10:36 -0700

    feat: custom integer only text field that only allow integer as input

---
+++ front/src/pages/modules/formikTextField/IntegerTextField.js
@@ -0,0 +1,35 @@
+import React, {
+  useCallback,
+} from 'react';
+import {
+  fieldToTextField,
+} from 'formik-material-ui';
+import {
+  TextField,
+} from '@material-ui/core';
+
+const IntegerTextField = (props) => {
+  const {
+    form: {setFieldValue},
+    field: {name},
+  } = props;
+  const onChange = useCallback(
+    (event) => {
+      const {value} = event.target;
+      setFieldValue(name, value ? value.replace(/\./g, '') : '');
+    },
+    [setFieldValue, name]
+  );
+  return <TextField
+          {...fieldToTextField(props)}
+          type="number"
+          step="1" 
+          onChange={onChange} onKeyDown={(e) => {
+            if (e.keyCode === 190) {
+              e.preventDefault()
+            }
+          }}
+        />;
+}
+
+export default IntegerTextField;
